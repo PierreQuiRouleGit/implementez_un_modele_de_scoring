fe383957a2c8ff726ad48ea26d5da7e23b88f218
Ternisien Pierre <pierre-ter@sfr.fr>
2023-04-13 23:44:56 +0200

    flask + jsonify

--- api_score.py
+++ api_score.py
@@ -1,43 +1,16 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.graph_objects as go
-import time
-
-import streamlit as st
-
-
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, learning_curve, StratifiedKFold
-from sklearn.metrics import *
-from imblearn.over_sampling import SMOTE
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn import tree
-from catboost import CatBoostClassifier
-from lightgbm import LGBMClassifier
-from xgboost import XGBClassifier
-from sklearn.metrics import roc_auc_score
-from sklearn.feature_selection import RFECV
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from flask import Flask, render_template, jsonify, request, flash, redirect, url_for
 
 import pickle
 
-from hyperopt import fmin, tpe, hp, STATUS_OK, Trials, space_eval
-from hyperopt.pyll import scope
 
-import mlflow
-from pyngrok import ngrok
-
-import shap
-import lime
-from lime import lime_tabular
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '11111111111'
 
 
 #data characteristics
-@st.cache_data
 def load_df():
     df = pd.read_csv("df_merge_sample.csv")
     return df
@@ -46,7 +19,6 @@ df = load_df()
 
 
 #data predict
-@st.cache_data
 def load_df_test_copy():
     df_test_copy = pd.read_csv("df_test_sample.csv")
     return df_test_copy
@@ -54,7 +26,6 @@ def load_df_test_copy():
 df_test_copy = load_df_test_copy()
 
 
-@st.cache_data
 def load_df_test():
     df_test = pd.read_csv("df_test_transformed_sample.csv")
     df_test.drop(columns=['Unnamed: 0'],inplace=True)
@@ -65,7 +36,6 @@ df_test = load_df_test()
 
 
 #best model
-@st.cache_resource
 def load_model():
     pickle_model = open('model.pkl', 'rb') 
     clf = pickle.load(pickle_model)
@@ -84,19 +54,31 @@ df.loc[df['TARGET'].isna(),'TARGET'] = 0.5
 
 proba = clf.predict_proba(df_test)
 
-proba = pd.DataFrame(proba)
+proba_data = pd.DataFrame(proba).round(2)
 
-proba.drop(columns=[0],inplace=True)
+proba_data.drop(columns=[0],inplace=True)
 
 id = pd.DataFrame(df_test_copy['SK_ID_CURR'])
 
-proba_id = [id,proba]
+proba_id = [id,proba_data]
 
 proba_id_concat = pd.concat(proba_id, axis=1, join='inner')
 
-st.table(proba_id_concat)
+
+@app.route('/')
+def home():
+    return 'Hello world!'
 
 
 
 
+@app.route('/credit/<id_client>')
+def credit(id_client):
+    
+    return jsonify(SK_ID_CURR=id_client,
+                score =proba_id_concat.loc[proba_id_concat['SK_ID_CURR']==int(id_client),1].values[0])
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
